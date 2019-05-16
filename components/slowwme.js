4506399b579661a43d211fc797017528d0e9255c
rohitdhamija <rohit.dhamija@gmail.com>
2019-05-16 17:41:43 +0530

    first commit

---
+++ components/slowwme.js
@@ -0,0 +1,23 @@
+'use strict';
+var sleep = require('thread-sleep');
+module.exports = {
+  metadata: () => ({
+    name: 'slowwme',
+    properties: {
+      sleepduration: { required: true, type: 'int' },
+    }
+  }),
+  invoke: (conversation, done) => {
+    // perform conversation tasks.
+    const { sleepduration } = conversation.properties();
+    var start = Date.now();
+    var res = sleep(sleepduration);
+    var end = Date.now();
+    var diff = end - start;  
+    // reply
+    conversation.logger().info(`I slept for ~${diff} milliseconds`);
+    conversation.transition();
+    conversation.keepTurn(true);  
+    done();
+  }
+};
